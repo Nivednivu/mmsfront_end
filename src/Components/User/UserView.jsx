3e31436dba7ade690a49ac3cd33634d7e110c1fb
Nivednivu <142215596+Nivednivu@users.noreply.github.com>
2025-06-04 13:56:19 +0530

    Update UserView.jsx

--- src/Components/User/UserView.jsx
+++ src/Components/User/UserView.jsx
@@ -217,7 +217,7 @@ try {
   gap:12px;
 }
             .query-table {
-              width: 637px;
+              width: 645px;
               border-collapse: collapse;
               font-size: 8px;
               border:0.5px solid black;
